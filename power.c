4166d34d881200733da1ac8b04fb1d12f7b180de
Daniel Erat <derat@chromium.org>
2017-12-05 14:32:52 -0800

    flashrom: Move lock file to /run/lock/power_override.
    Make flashrom create a lockfile at
    /run/lock/power_override/flashrom.lock rather than
    /run/lock/flashrom_powerd.lock so that powerd doesn't need
    to special-case the file's location.
    
    BUG=chromium:784651
    BRANCH=None
    CQ-DEPEND=Icdc2983707c19480abbde020276c0e2b4ec2e958
    TEST=ran "flashrom -w <file>" and verified that lockfile was
         created at /run/lock/power_override/flashrom.lock, then
         moved the power_override dir away and checked that
         flashrom doesn't log an error
    
    Change-Id: I5b85e13c3a71ab33a08add33c2b815d6bfd49208
    Signed-off-by: Daniel Erat <derat@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/809853
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>

--- power.c
+++ power.c
@@ -28,42 +28,42 @@
 #include <limits.h>
 
 #include "flash.h"	/* for msg_* */
-#include "locks.h"	/* for SYSTEM_LOCKFILE_DIR */
 #include "power.h"
 
 /*
- * Path to a file containing flashrom's PID. While present, powerd avoids
- * suspending or shutting down the system.
+ * Path to directory and file containing flashrom's PID.
+ * While present, powerd avoids suspending or shutting down the system.
  */
-static const char powerd_lock_file_name[] = "flashrom_powerd.lock";
+#define POWERD_LOCK_DIR		"/run/lock/power_override"
+#define POWERD_LOCK_FILE	POWERD_LOCK_DIR "/flashrom.lock"
 
 int disable_power_management()
 {
 	FILE *lock_file = NULL;
 	int rc = 0;
-	char lock_file_path[PATH_MAX];
+
+	/* Don't do anything if powerd isn't expected to run. */
+	if (access(POWERD_LOCK_DIR, F_OK) != 0) {
+		return 0;
+	}
 
 	msg_pdbg("%s: Disabling power management.\n", __func__);
 
-	if (snprintf(lock_file_path, sizeof(lock_file_path), "%s/%s",
-			SYSTEM_LOCKFILE_DIR, powerd_lock_file_name) < 0)
-		return 1;
-
-	if (!(lock_file = fopen(lock_file_path, "w"))) {
+	if (!(lock_file = fopen(POWERD_LOCK_FILE, "w"))) {
 		msg_perr("%s: Failed to open %s for writing: %s\n",
-			__func__, lock_file_path, strerror(errno));
+			__func__, POWERD_LOCK_FILE, strerror(errno));
 		return 1;
 	}
 
 	if (fprintf(lock_file, "%ld", (long)getpid()) < 0) {
 		msg_perr("%s: Failed to write PID to %s: %s\n",
-			__func__, lock_file_path, strerror(errno));
+			__func__, POWERD_LOCK_FILE, strerror(errno));
 		rc = 1;
 	}
 
 	if (fclose(lock_file) != 0) {
 		msg_perr("%s: Failed to close %s: %s\n",
-			__func__, lock_file_path, strerror(errno));
+			__func__, POWERD_LOCK_FILE, strerror(errno));
 	}
 	return rc;
 
@@ -72,18 +72,18 @@ int disable_power_management()
 int restore_power_management()
 {
 	int result = 0;
-	char lock_file_path[PATH_MAX];
+
+	/* Don't do anything if powerd isn't expected to run. */
+	if (access(POWERD_LOCK_DIR, F_OK) != 0) {
+		return 0;
+	}
 
 	msg_pdbg("%s: Re-enabling power management.\n", __func__);
 
-	if (snprintf(lock_file_path, sizeof(lock_file_path), "%s/%s",
-			SYSTEM_LOCKFILE_DIR, powerd_lock_file_name) < 0)
-		return 1;
-
-	result = unlink(lock_file_path);
+	result = unlink(POWERD_LOCK_FILE);
 	if (result != 0 && errno != ENOENT)  {
 		msg_perr("%s: Failed to unlink %s: %s\n",
-			__func__, lock_file_path, strerror(errno));
+			__func__, POWERD_LOCK_FILE, strerror(errno));
 		return 1;
 	}
 	return 0;
