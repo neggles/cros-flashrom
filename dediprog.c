cc0bf44bc41fe3d795b9b899981c62793ab6b7d4
Edward O'Callaghan <quasisec@chromium.org>
2019-02-27 22:35:44 +1100

    dediprog: Add SF200 support
    BUG=chromium:478356
    BRANCH=none
    TEST=still builds
    
    Change-Id: I89b05f3e40ae3f758d1f9051e7822eb31b7c760b
    Signed-off-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1492818
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>

--- dediprog.c
+++ dediprog.c
@@ -51,6 +51,7 @@ static int dediprog_firmwareversion = FIRMWARE_VERSION(0, 0, 0);
 enum dediprog_devtype {
 	DEV_UNKNOWN		= 0,
 	DEV_SF100		= 100,
+	DEV_SF200		= 200,
 	DEV_SF600		= 600,
 };
 
@@ -176,7 +177,11 @@ static enum protocol protocol(void)
 	/* Firmware version < 5.0.0 is handled explicitly in some cases. */
 	switch (dediprog_devicetype) {
 	case DEV_SF100:
-		return dediprog_firmwareversion >= FIRMWARE_VERSION(5, 5, 0);
+	case DEV_SF200:
+		if (dediprog_firmwareversion < FIRMWARE_VERSION(5, 5, 0))
+			return PROTOCOL_V1;
+		else
+			return PROTOCOL_V2;
 	case DEV_SF600:
 		if (dediprog_firmwareversion < FIRMWARE_VERSION(6, 9, 0))
 			return PROTOCOL_V1;
@@ -717,10 +722,12 @@ static int dediprog_check_devicestring(void)
 	msg_pdbg("Found a %s\n", buf);
 	if (memcmp(buf, "SF100", 0x5) == 0)
 		dediprog_devicetype = DEV_SF100;
+	else if (memcmp(buf, "SF200", 0x5) == 0)
+		dediprog_devicetype = DEV_SF200;
 	else if (memcmp(buf, "SF600", 0x5) == 0)
 		dediprog_devicetype = DEV_SF600;
 	else {
-		msg_perr("Device not a SF100 or SF600!\n");
+		msg_perr("Device not a SF100, SF200, or SF600!\n");
 		return 1;
 	}
 
@@ -1128,13 +1135,17 @@ int dediprog_init(void)
 			return 1;
 	}
 
-	/* SF100 firmware exposes only one endpoint for in/out, SF600 firmware
-           exposes separate endpoints for in and out. */
+	/* SF100/SF200 uses one in/out endpoint, SF600 uses separate in/out endpoints */
 	dediprog_in_endpoint = 2;
-	if (dediprog_devicetype == DEV_SF100)
+	switch (dediprog_devicetype) {
+	case DEV_SF100:
+	case DEV_SF200:
 		dediprog_out_endpoint = 2;
-	else
+		break;
+	default:
 		dediprog_out_endpoint = 1;
+		break;
+	}
 
 
 	/* Set some LEDs as soon as possible to indicate activity.
