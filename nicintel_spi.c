1aa8e9ae4831852ab631d7b8664694ef3de453a2
Edward O'Callaghan <quasisec@chromium.org>
2019-08-02 17:16:47 +1000

    nicintel_spi: Use a helpful macro to grab bits
    Aligns with what upstream does, looks a little more tidy.
    
    BUG=chromium:478356
    BRANCH=none
    TEST=none
    
    Change-Id: Ia5c35d1020060b8152f314d91bd91f180e84408b
    Signed-off-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/1731368
    Reviewed-by: Sam McNally <sammc@chromium.org>

--- nicintel_spi.c
+++ nicintel_spi.c
@@ -70,6 +70,8 @@
 // #define FL_BUSY	30
 // #define FL_ER	31
 
+#define BIT(x) (1<<(x))
+
 static uint8_t *nicintel_spibar;
 
 const struct dev_entry nics_intel_spi[] = {
@@ -99,11 +101,11 @@ static void nicintel_request_spibus(void)
 	uint32_t tmp;
 
 	tmp = pci_mmio_readl(nicintel_spibar + FLA);
-	tmp |= 1 << FL_REQ;
+	tmp |= BIT(FL_REQ);
 	pci_mmio_writel(tmp, nicintel_spibar + FLA);
 
 	/* Wait until we are allowed to use the SPI bus. */
-	while (!(pci_mmio_readl(nicintel_spibar + FLA) & (1 << FL_GNT))) ;
+	while (!(pci_mmio_readl(nicintel_spibar + FLA) & BIT(FL_GNT))) ;
 }
 
 static void nicintel_release_spibus(void)
@@ -111,7 +113,7 @@ static void nicintel_release_spibus(void)
 	uint32_t tmp;
 
 	tmp = pci_mmio_readl(nicintel_spibar + FLA);
-	tmp &= ~(1 << FL_REQ);
+	tmp &= ~BIT(FL_REQ);
 	pci_mmio_writel(tmp, nicintel_spibar + FLA);
 }
 
@@ -120,7 +122,7 @@ static void nicintel_bitbang_set_cs(int val)
 	uint32_t tmp;
 
 	tmp = pci_mmio_readl(nicintel_spibar + FLA);
-	tmp &= ~(1 << FL_CS);
+	tmp &= ~BIT(FL_CS);
 	tmp |= (val << FL_CS);
 	pci_mmio_writel(tmp,  nicintel_spibar + FLA);
 }
@@ -130,7 +132,7 @@ static void nicintel_bitbang_set_sck(int val)
 	uint32_t tmp;
 
 	tmp = pci_mmio_readl(nicintel_spibar + FLA);
-	tmp &= ~(1 << FL_SCK);
+	tmp &= ~BIT(FL_SCK);
 	tmp |= (val << FL_SCK);
 	pci_mmio_writel(tmp, nicintel_spibar + FLA);
 }
@@ -140,7 +142,7 @@ static void nicintel_bitbang_set_mosi(int val)
 	uint32_t tmp;
 
 	tmp = pci_mmio_readl(nicintel_spibar + FLA);
-	tmp &= ~(1 << FL_SI);
+	tmp &= ~BIT(FL_SI);
 	tmp |= (val << FL_SI);
 	pci_mmio_writel(tmp, nicintel_spibar + FLA);
 }
