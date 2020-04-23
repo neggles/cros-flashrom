95f1e5331afd12148a17526a29db3cb5ccb4aec4
Edward O'Callaghan <quasisec@google.com>
2020-04-23 14:02:25 +1000

    print.c: Resync with upstream
    Pulls in a memory leak fix from upstream.
    
    BUG=b:154777318
    BRANCH=none
    TEST=builds
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: I17da991df08a5fd3987a535a79492cfe30beeed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2160950
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- print.c
+++ print.c
@@ -192,6 +192,7 @@ static int print_supported_chips(int host_controller)
 		dev = malloc(strlen(chip->name) + 1);
 		if (dev == NULL) {
 			msg_gerr("Out of memory!\n");
+			free(ven);
 			return 1;
 		}
 		strcpy(dev, chip->name);
@@ -489,7 +490,7 @@ int print_supported(void)
 	       programmer_table[PROGRAMMER_INTERNAL].name);
 	print_supported_chipsets();
 	msg_ginfo("\n");
-	print_supported_boards_helper(boards_known, "boards");
+	print_supported_boards_helper(boards_known, "mainboards");
 	msg_ginfo("\n");
 	print_supported_boards_helper(laptops_known, "mobile devices");
 #endif
@@ -688,6 +689,7 @@ const struct board_info boards_known[] = {
 	B("ASUS",	"P4P800-E Deluxe",	OK, "https://www.asus.com/Motherboards/Intel_Socket_478/P4P800E_Deluxe/", NULL),
 	B("ASUS",	"P4P800-VM",		OK, "https://www.asus.com/Motherboards/Intel_Socket_478/P4P800VM/", NULL),
 	B("ASUS",	"P4P800-X",		OK, "https://www.asus.com/Motherboards/Intel_Socket_478/P4P800X/", NULL),
+	B("ASUS",	"P4P800SE",		OK, "https://www.asus.com/supportonly/P4P800 SE/", NULL),
 	B("ASUS",	"P4PE-X/TE",		NT, "https://www.asus.com/999/html/events/mb/socket478/p4pe-x-te/overview.htm", NULL),
 	B("ASUS",	"P4S533-X",		OK, NULL, NULL),
 	B("ASUS",	"P4S800-MX",		OK, "https://www.asus.com/Motherboards/Intel_Socket_478/P4S800MX/", NULL),
