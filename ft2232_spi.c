d520d3b45a046cf8c68aa0c0e3d7191e3ebcaa6b
Nicolas Boichat <drinkcat@chromium.org>
2020-05-18 08:26:20 +0800

    ft2232_spi: Add missing new line after deprecation warnings
    BRANCH=master
    BUG=b:145175076
    TEST=none
    
    Change-Id: I27927ea84b554b6754d1da42f81503ea1314a9c4
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2206095
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>

--- ft2232_spi.c
+++ ft2232_spi.c
@@ -288,19 +288,19 @@ int ft2232_spi_init(void)
 			ft2232_vid = GOOGLE_VID;
 			ft2232_type = GOOGLE_SERVO_PID;
 			channel_count = 2;
-			msg_pwarn("Warning: Use 'google-servo' instead!");
+			msg_pwarn("Warning: Use 'google-servo' instead!\n");
 		} else if (!strcasecmp(arg, "servo-v2")) {
 			ft2232_vid = GOOGLE_VID;
 			ft2232_type = GOOGLE_SERVO_V2_PID1;
 			channel_count = 2;
-			msg_pwarn("Warning: Use 'google-servo-v2' instead!");
+			msg_pwarn("Warning: Use 'google-servo-v2' instead!\n");
 			/* Default divisor is too fast, and chip ID fails */
 			divisor = 6;
 		} else if (!strcasecmp(arg, "servo-v2-legacy")) {
 			ft2232_vid = GOOGLE_VID;
 			ft2232_type = GOOGLE_SERVO_V2_PID0;
 			channel_count = 2;
-			msg_pwarn("Warning: Use 'google-servo-v2-legacy' instead!");
+			msg_pwarn("Warning: Use 'google-servo-v2-legacy' instead!\n");
 		/* ^^ WARNING: deprecated ^^ */
 		} else if (!strcasecmp(arg, "flyswatter")) {
 			ft2232_type = FTDI_FT2232H_PID;
