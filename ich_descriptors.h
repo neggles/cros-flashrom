1229b21f7ffe1b8169f43ef9e6b8cd3f8eec3ec9
Edward O'Callaghan <quasisec@google.com>
2020-07-05 13:28:03 +1000

    ichspi_{desc}.[h,c]: Adapt getFCBA_component_density() to use ich gen
    Allow getFCBA_component_density() to align with upstream
    where it is parametric on the ich generation.
    
    Has origin in upstream `commit 2ba9f6ebe56b208a1fb0b0ce5edf81097a0158be`.
    
    BUG=b:156870835,b:155142726,b:139255749,b:139258259
    BRANCH=none
    TEST=builds
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: I9cb3a333782ffd62748e96b3631cef9c2be63f3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2215933
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- ich_descriptors.h
+++ ich_descriptors.h
@@ -586,7 +586,7 @@ int read_ich_descriptors_from_dump(const uint32_t *const dump, const size_t len,
 #else /* ICH_DESCRIPTORS_FROM_DUMP */
 
 int read_ich_descriptors_via_fdo(enum ich_chipset cs, void *spibar, struct ich_descriptors *desc);
-int getFCBA_component_density(const struct ich_descriptors *desc, uint8_t idx);
+int getFCBA_component_density(enum ich_chipset cs, const struct ich_descriptors *desc, uint8_t idx);
 
 #endif /* ICH_DESCRIPTORS_FROM_DUMP */
 #endif /* __ICH_DESCRIPTORS_H__ */
