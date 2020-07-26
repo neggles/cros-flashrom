23e7c4e19c471d7c7b90407d853c39a4f5d506e9
Edward O'Callaghan <quasisec@google.com>
2020-07-26 17:16:39 +1000

    BACKPORT: Refine messages related to erase/write recovery
    We are more verbose inside erase_and_write_flash() although it does not
    matter as much as at the end of the whole process in doit().
    
    New output for the non-fatal (i.e. read-protected + successful recovery
    read) case:
    
    Reading old flash chip contents... done.
    Erasing and writing flash chip... spi_block_erase_d8 failed during
    command execution at address 0x8000
    Reading current flash chip contents... done. spi_chip_erase_c7 failed
    during command execution
    FAILED!
    Uh oh. Erase/write failed. Checking if anything has changed.
    Reading current flash chip contents... done.
    Good, writing to the flash chip apparently didn't do anything.
    Please check the connections (especially those to write protection pins)
    between
    the programmer and the flash chip. If you think the error is caused by
    flashrom
    please report this on IRC at chat.freenode.net (channel #flashrom) or
    mail flashrom@flashrom.org, thanks!
    
    Corresponding to flashrom svn r1790.
    
    Original-Signed-off-by: Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>
    Original-Acked-by: Stefan Tauner <stefan.tauner@alumni.tuwien.ac.at>
    (cherry picked from commit a58f6e9b547fd1930a673c526db4476f2ccfaa77)
    
    Actually in truth this was manually back ported by hand.
    
    BUG=b:139101642,b:162158026
    BRANCH=none
    TEST=builds
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: I23acf47f298c56a676748b3bd3855cab11aa7189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2319510
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>

--- flashrom.c
+++ flashrom.c
@@ -1766,7 +1766,7 @@ int erase_and_write_flash(struct flashctx *flash,
 
 void nonfatal_help_message(void)
 {
-	msg_gerr("Writing to the flash chip apparently didn't do anything.\n"
+	msg_gerr("Good, writing to the flash chip apparently didn't do anything.\n"
 		"This means we have to add special support for your board, "
 		  "programmer or flash chip.\n"
 		"Please report this on IRC at irc.freenode.net (channel "
@@ -2230,17 +2230,18 @@ int doit(struct flashctx *flash, int force, const char *filename, int read_it,
 		}
 
 		if (erase_and_write_flash(flash, descriptor)) {
-			msg_cerr("Uh oh. Erase/write failed. Checking if "
-				 "anything changed.\n");
+			msg_cerr("Uh oh. Erase/write failed. Checking if anything changed.\n");
+			msg_cinfo("Reading current flash chip contents... ");
 			if (!read_flash(flash, newcontents, 0, size)) {
+				msg_cinfo("done.\n");
 				if (!memcmp(oldcontents, newcontents, size)) {
-					msg_cinfo("Good. It seems nothing was "
-						  "changed.\n");
 					nonfatal_help_message();
 					ret = 1;
 					goto out;
 				}
-			}
+				msg_cerr("Apparently at least some data has changed.\n");
+			} else
+				msg_cerr("Can't even read anymore!\n");
 			emergency_help_message();
 			ret = 1;
 			goto out;
