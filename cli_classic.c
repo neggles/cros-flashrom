neggles/cros-flashrom

commit 79357b3d3acfb5bf4e001fbdae29353699362ef1

flashrom.c: Use map_flash()/unmap_flash() from upstream
Move flash address space logic into their own functions
and keep their state around for the life-time of flashctx
as per-upstream.

This is essentially close to the upstream commit 4e32ec19b124a.

Note that the mapping leak in cli_classic is later removed in
the upstream commit 899e4ec810 but not squashed here for various
reasons.

BUG=b:165100282
BRANCH=none
TEST=builds

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: I9e352f9b306864a7b112675e2f8365aaacdc0b16
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2359941
Tested-by: Edward O'Callaghan <quasisec@chromium.org>
Reviewed-by: Sam McNally <sammc@chromium.org>
Commit-Queue: Sam McNally <sammc@chromium.org>
Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>

cli_classic.c
@@ -687,8 +687,16 @@ int main(int argc, char *argv[])
 				ret = 1;
 				goto out_shutdown;
 			}
+			if (map_flash(&flashes[0]) != 0) {
+				free(flashes[0].chip);
+				ret = 1;
+				goto out_shutdown;
+			}
 			msg_cinfo("Please note that forced reads most likely contain garbage.\n");
-			return read_flash_to_file(&flashes[0], filename);
+			ret = read_flash_to_file(&flashes[0], filename);
+			unmap_flash(&flashes[0]);
+			free(flashes[0].chip);
+			goto out_shutdown;
 		}
 		ret = 1;
 		goto out_shutdown;
@@ -785,6 +793,12 @@ int main(int argc, char *argv[])
 				goto out_shutdown;
 			}
 		}
+	}
+
+	/* Map the selected flash chip again. */
+	if (map_flash(fill_flash) != 0) {
+		ret = 1;
+		goto out_shutdown;
 	}
 
 	/* Always verify write operations unless -n is used. */
@@ -959,6 +973,8 @@ int main(int argc, char *argv[])
 	}
 
 	msg_ginfo("%s\n", ret ? "FAILED" : "SUCCESS");
+
+	unmap_flash(fill_flash);
 out_shutdown:
 	programmer_shutdown();  /* must be done after chip_restore() */
 #if USE_BIG_LOCK == 1

82802ab.c
@@ -79,9 +79,6 @@ int probe_82802ab(struct flashctx *flash)
 	if (id1 != flash->chip->manufacture_id || id2 != flash->chip->model_id)
 		return 0;
 
-	if (flash->chip->feature_bits & FEATURE_REGISTERMAP)
-		map_flash_registers(flash);
-
 	return 1;
 }
 

flash.h
@@ -266,9 +266,14 @@ struct flashchip {
 /* struct flashctx must always contain struct flashchip at the beginning. */
 struct flashctx {
 	struct flashchip *chip;
-
+	/* FIXME: The memory mappings should be saved in a more structured way. */
+	/* The physical_* fields store the respective addresses in the physical address space of the CPU. */
+	uintptr_t physical_memory;
+	/* The virtual_* fields store where the respective physical address is mapped into flashrom's address
+	 * space. A value equivalent to (chipaddr)ERROR_PTR indicates an invalid mapping (or none at all). */
 	chipaddr virtual_memory;
 	/* Some flash devices have an additional register space; semantics are like above. */
+	uintptr_t physical_registers;
 	chipaddr virtual_registers;
 	struct registered_master *mst;
 
@@ -339,7 +344,8 @@ extern const char flashrom_version[];
 extern const char *chip_to_probe;
 char *flashbuses_to_text(enum chipbustype bustype);
 extern enum chipbustype buses_supported;
-void map_flash_registers(struct flashctx *flash);
+int map_flash(struct flashctx *flash);
+void unmap_flash(struct flashctx *flash);
 int read_memmapped(struct flashctx *flash, uint8_t *buf, unsigned int start, unsigned int len);
 int erase_flash(struct flashctx *flash);
 int probe_flash(struct registered_master *mst, int startchip, struct flashctx *fill_flash, int force);

flashrom.c
@@ -514,6 +514,7 @@ void *programmer_map_flash_region(const char *descr, unsigned long phys_addr,
 void programmer_unmap_flash_region(void *virt_addr, size_t len)
 {
 	programmer_table[programmer].unmap_flash_region(virt_addr, len);
+	msg_gspew("%s: unmapped 0x%0*" PRIxPTR "\n", __func__, PRIxPTR_WIDTH, (uintptr_t)virt_addr);
 }
 
 void chip_writeb(const struct flashctx *flash, uint8_t val, chipaddr addr)
@@ -560,14 +561,6 @@ void programmer_delay(unsigned int usecs)
 {
 	if (usecs > 0)
 		programmer_table[programmer].delay(usecs);
-}
-
-void map_flash_registers(struct flashctx *flash)
-{
-	size_t size = flash->chip->total_size * 1024;
-	/* Flash registers live 4 MByte below the flash. */
-	/* FIXME: This is incorrect for nonstandard flashbase. */
-	flash->virtual_registers = (chipaddr)programmer_map_flash_region("flash chip registers", (0xFFFFFFFF - 0x400000 - size + 1), size);
 }
 
 int read_memmapped(struct flashctx *flash, uint8_t *buf, unsigned int start, int unsigned len)
@@ -1133,6 +1126,61 @@ int check_max_decode(enum chipbustype buses, uint32_t size)
 	return 1;
 }
 
+void unmap_flash(struct flashctx *flash)
+{
+	if (flash->virtual_registers != (chipaddr)ERROR_PTR) {
+		programmer_unmap_flash_region((void *)flash->virtual_registers, flash->chip->total_size * 1024);
+		flash->physical_registers = 0;
+		flash->virtual_registers = (chipaddr)ERROR_PTR;
+	}
+
+	if (flash->virtual_memory != (chipaddr)ERROR_PTR) {
+		programmer_unmap_flash_region((void *)flash->virtual_memory, flash->chip->total_size * 1024);
+		flash->physical_memory = 0;
+		flash->virtual_memory = (chipaddr)ERROR_PTR;
+	}
+}
+
+int map_flash(struct flashctx *flash)
+{
+	/* Init pointers to the fail-safe state to distinguish them later from legit values. */
+	flash->virtual_memory = (chipaddr)ERROR_PTR;
+	flash->virtual_registers = (chipaddr)ERROR_PTR;
+
+	/* FIXME: This avoids mapping (and unmapping) of flash chip definitions with size 0.
+	 * These are used for various probing-related hacks that would not map successfully anyway and should be
+	 * removed ASAP. */
+	if (flash->chip->total_size == 0)
+		return 0;
+
+	const chipsize_t size = flash->chip->total_size * 1024;
+	uintptr_t base = flashbase ? flashbase : (0xffffffff - size + 1);
+	void *addr = programmer_map_flash_region(flash->chip->name, base, size);
+	if (addr == ERROR_PTR) {
+		msg_perr("Could not map flash chip %s at 0x%0*" PRIxPTR ".\n",
+			 flash->chip->name, PRIxPTR_WIDTH, base);
+		return 1;
+	}
+	flash->physical_memory = base;
+	flash->virtual_memory = (chipaddr)addr;
+
+	/* FIXME: Special function registers normally live 4 MByte below flash space, but it might be somewhere
+	 * completely different on some chips and programmers, or not mappable at all.
+	 * Ignore these problems for now and always report success. */
+	if (flash->chip->feature_bits & FEATURE_REGISTERMAP) {
+		base = 0xffffffff - size - 0x400000 + 1;
+		addr = programmer_map_flash_region("flash chip registers", base, size);
+		if (addr == ERROR_PTR) {
+			msg_pdbg2("Could not map flash chip registers %s at 0x%0*" PRIxPTR ".\n",
+				 flash->chip->name, PRIxPTR_WIDTH, base);
+			return 0;
+		}
+		flash->physical_registers = base;
+		flash->virtual_registers = (chipaddr)addr;
+	}
+	return 0;
+}
+
 /*
  * Return a string corresponding to the bustype parameter.
  * Memory is obtained with malloc() and must be freed with free() by the caller.
@@ -1169,8 +1217,6 @@ char *flashbuses_to_text(enum chipbustype bustype)
 int probe_flash(struct registered_master *mst, int startchip, struct flashctx *flash, int force)
 {
 	const struct flashchip *chip, *flash_list;
-	unsigned long base = 0;
-	char location[64];
 	uint32_t size;
 	enum chipbustype buses_common;
 	char *tmp;
@@ -1216,8 +1262,8 @@ int probe_flash(struct registered_master *mst, int startchip, struct flashctx *f
 		memcpy(flash->chip, chip, sizeof(struct flashchip));
 		flash->mst = mst;
 
-		base = flashbase ? flashbase : (0xffffffff - size + 1);
-		flash->virtual_memory = (chipaddr)programmer_map_flash_region("flash chip", base, size);
+		if (map_flash(flash) != 0)
+			goto notfound;
 
 		if (force)
 			break;
@@ -1236,8 +1282,7 @@ int probe_flash(struct registered_master *mst, int startchip, struct flashctx *f
 			break;
 
 notfound:
-		if (size)
-			programmer_unmap_flash_region((void *)flash->virtual_memory, size);
+		unmap_flash(flash);
 		free(flash->chip);
 		flash->chip = NULL;
 	}
@@ -1245,19 +1290,19 @@ notfound:
 	if (!chip || !chip->name)
 		return -1;
 
-#if CONFIG_INTERNAL == 1
-	if (programmer_table[programmer].map_flash_region == physmap)
-		snprintf(location, sizeof(location), "at physical address 0x%lx", base);
-	else
-#endif
-		snprintf(location, sizeof(location), "on %s", programmer_table[programmer].name);
 
 	tmp = flashbuses_to_text(chip->bustype);
-	msg_cdbg("%s %s flash chip \"%s\" (%d kB, %s) %s.\n",
+	msg_cdbg("%s %s flash chip \"%s\" (%d kB, %s) \n",
 		 force ? "Assuming" : "Found", flash->chip->vendor,
-		 flash->chip->name, flash->chip->total_size, tmp,
-		 location);
+		 flash->chip->name, flash->chip->total_size, tmp);
 	free(tmp);
+#if CONFIG_INTERNAL == 1
+	if (programmer_table[programmer].map_flash_region == physmap)
+		msg_cinfo("mapped at physical address 0x%0*" PRIxPTR ".\n",
+			  PRIxPTR_WIDTH, flash->physical_memory);
+	else
+#endif
+		msg_cinfo("on %s.\n", programmer_table[programmer].name);
 
 	/* Flash registers will not be mapped if the chip was forced. Lock info
 	 * may be stored in registers, so avoid lock info printing.
@@ -1265,6 +1310,9 @@ notfound:
 	if (!force)
 		if (flash->chip->printlock)
 			flash->chip->printlock(flash);
+
+	/* Get out of the way for later runs. */
+	unmap_flash(flash);
 
 	/* Return position of matching chip. */
 	return chip - flash_list;

jedec.c
@@ -162,9 +162,6 @@ int probe_jedec_29gl(struct flashctx *flash)
 	if (man_id != chip->manufacture_id || dev_id != chip->model_id)
 		return 0;
 
-	if (chip->feature_bits & FEATURE_REGISTERMAP)
-		map_flash_registers(flash);
-
 	return 1;
 }
 
@@ -280,9 +277,6 @@ static int probe_jedec_common(struct flashctx *flash, unsigned int mask)
 	msg_cdbg("\n");
 	if (largeid1 != chip->manufacture_id || largeid2 != chip->model_id)
 		return 0;
-
-	if (flash->chip->feature_bits & FEATURE_REGISTERMAP)
-		map_flash_registers(flash);
 
 	return 1;
 }

commit d206c960082ed9446fa3e1c60d8e19a94be51ccb

cli_classic.c: Fix opaque device mmap failures of chips
A regression was introduced in commit
79357b3d3acfb5bf4e001fbdae29353699362ef1
that impacts a number of DUT's both ARM and certain Intel boards
that happen to use the opaque path.

BUG=b:168933137,b:168862659
BRANCH=none
TEST=repro on Hana and fixed issue.
     Fei Shao - Also verified on Asurada.

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: Idc21ebbb29b7ec9476c0594dcdecc1728ac74f27
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2418522
Reviewed-by: Sam McNally <sammc@chromium.org>
Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
Tested-by: Nicolas Boichat <drinkcat@chromium.org>
Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>

cli_classic.c
@@ -795,12 +795,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	/* Map the selected flash chip again. */
-	if (map_flash(fill_flash) != 0) {
-		ret = 1;
-		goto out_shutdown;
-	}
-
 	/* Always verify write operations unless -n is used. */
 	if (write_it && !dont_verify_it)
 		if (!verify_it) verify_it = VERIFY_FULL;
@@ -974,7 +968,6 @@ int main(int argc, char *argv[])
 
 	msg_ginfo("%s\n", ret ? "FAILED" : "SUCCESS");
 
-	unmap_flash(fill_flash);
 out_shutdown:
 	programmer_shutdown();  /* must be done after chip_restore() */
 #if USE_BIG_LOCK == 1