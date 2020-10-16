07d73808c4f758a9962d6a33329b536abaec9106
Edward O'Callaghan <quasisec@google.com>
2020-10-16 15:06:18 +1100

    Makefile: Sync script func and scripts
    Unfortunately this is a large commit in order to be
    atomic, such is the nature of gnu-make++bash.
    
    The aim here is to resync our Makefile with upstreams
    and the focus of this commit is specifically around
    the various scripts and func that burn in version numbers
    and check OS features.
    
    Squash in,
      util/getrevision.sh: Take upstreams version
    
      util/z60_flashrom.rules: Import from upstream
      util/list_yet_unsupported_chips.sh: Import from upstream
      util/flashrom_partial_write_test.sh: Import from upstream
      util/git-hooks: Import from upstream
    
    BUG=b:171014265
    BRANCH=none
    TEST=emerge and test on DUT that cli gives correct strings.
    
    Disallow-Recycled-Builds: test-failures
    Cq-Depend: chromium:2491623
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: Ieb34bb7b03cec62ba7846b1ee180686a0cb5c41d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2477704
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>

--- util/getrevision.sh
+++ util/getrevision.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+# NB: Supposed to be POSIX compatible but at least the usage of 'local' is not.
 #
 # This file is part of the flashrom project.
 #
+# Copyright (C) 2005 coresystems GmbH <stepan@coresystems.de>
 # Copyright (C) 2009,2010 Carl-Daniel Hailfinger
-# Copyright (C) 2011 Chromium OS Authors
+# Copyright (C) 2010 Chromium OS Authors
+# Copyright (C) 2013-2016 Stefan Tauner
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,154 +18,221 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-#
-# getrevision.sh: Get version / revision info. Currently, only Git
-# and Subversion are supported.
-#
-# *_revision:   Echo the latest source code revision (git hash or svn revision).
-#
-# *_server:     Echo the server from where the source code was checked out.
-#
-# *_timestamp:  Echo the timestamp of most recent modification. If the sources
-#               are pristine, the timestamp will correspond to the most recently
-#               committed change, expressed in UTC. If there are local
-#               modifications, the timestamp will correspond to compilation
-#               time and a '+' is added to denote uncommitted changes.
-#
 
-git_revision() {
-  echo $(git log --pretty=format:'%h' -n 1)
+EXIT_SUCCESS=0
+EXIT_FAILURE=1
+
+# Make sure we don't get translated output
+export LC_ALL=C
+# nor local times or dates
+export TZ=UTC0
+
+# Helper functions
+# First argument is the path to inspect (usually optional; without
+# it the whole repository will be considered)
+git_has_local_changes() {
+	git update-index -q --refresh >/dev/null
+	! git diff-index --quiet HEAD -- "$1"
 }
 
-git_server() {
-  # Note: This may not work as expected if multiple remotes are fetched from.
-  echo $(git remote -v | \
-         awk '/fetch/ {split($2, pieces, "//"); print pieces[2]; exit 0}')
+git_last_commit() {
+	# git rev-parse --short HEAD would suffice if repository as a whole is of interest (no $1)
+	git log --pretty=format:"%h" -1 -- "$1"
 }
 
-git_timestamp() {
-  local date_format="+%b %d %Y %H:%M:%S"
-  local timestamp
-
-  # are there local changes in the client?
-  if git status | \
-    egrep '^# Change(s to be committed|d but not updated):$' > /dev/null
-  then
-    timestamp=$(date "${date_format} +")
-  else
-    # No local changes, get date of the last log record.
-    timestamp=$(git log --pretty=format:'%ct' -n 1 | \
-                awk '{print strftime("%b %d %Y %H:%M:%S UTC", $1, 1)}')
-  fi
-
-  echo "${timestamp}"
+git_is_file_tracked() {
+	git ls-files --error-unmatch -- "$1" >/dev/null 2>&1
 }
 
-svn_revision() {
-  local revision=""
-
-  # Try both "svnversion" and "svn info"
-  revision=$(LC_ALL=C svnversion -cn . 2>/dev/null | \
-             sed -e "s/.*://" -e "s/\([0-9]*\).*/\1/" | grep "[0-9]")
-
-  if [ -z "${revision}" ]; then
-    revision=$(LC_ALL=C svn info . 2>/dev/null | \
-               awk '/^Revision:/ {print $2 }' | grep "[0-9]")
-  fi
-
-  # Prepend revision with 'r'
-  echo "r${revision}"
+is_file_tracked() {
+	git_is_file_tracked "$1"
 }
 
-svn_server() {
-  echo $(LC_ALL=C svn info . 2>/dev/null | awk '/^URL:/ {print $2 }')
+# Tries to find a remote source for the changes committed locally.
+# This includes the URL of the remote repository including the last commit and a suitable branch name.
+# Takes one optional argument: the path to inspect
+git_url() {
+	last_commit=$(git_last_commit "$1")
+	# get all remote branches containing the last commit (excluding origin/HEAD)
+	branches=$(git branch -r --contains $last_commit | sed '/\//!d;/.*->.*/d;s/[\t ]*//')
+	if [ -z "$branches" ] ; then
+		echo "No remote branch contains a suitable commit">&2
+		return
+	fi
+
+	# find "nearest" branch
+	local mindiff=9000
+	local target=
+	for branch in $branches ; do
+		curdiff=$(git rev-list --count $last_commit..$branch)
+		if [ $curdiff -ge $mindiff ] ; then
+			continue
+		fi
+		mindiff=$curdiff
+		target=$branch
+	done
+
+	echo "$(git ls-remote --exit-code --get-url ${target%/*}) ${target#*/}"
 }
 
-svn_timestamp() {
-  local date_format="+%b %d %Y %H:%M:%S"
-  local timestamp
+# Returns a string indicating where others can get the current source code (excluding uncommitted changes).
+# Takes one optional argument: the path to inspect
+scm_url() {
+	local url=
 
-  # Whitelist most of the characters that appear on the left of
-  # "svn status" output, with the notable exceptions being '?' (Not
-  # under version control) and 'X'(item is an externals definition)
-  if [ -n "$(LC_ALL=C svn status . | grep '^ *[ADMRCI\!L+SKOTB\*].*')" ]; then
-    timestamp=$(date "${date_format} +")
-  else
-    local last_commit_date=$(svn info . | \
-      grep "Last Changed Date:" | awk '{print $4" "$5" "$6}')
-    timestamp=$(date --utc --date "${last_commit_date}" "${date_format} UTC")
-  fi
+	if git_is_file_tracked "$1" ; then
+		url="$(git_url "$1")"
+	else
+		return ${EXIT_FAILURE}
+	fi
 
-  echo "${timestamp}"
+	echo "${url}"
 }
 
-#
-# Determine which SCM we're using and choose the appropriate functions
-#
-IS_GIT=0
-IS_SVN=0
+# Retrieve timestamp since last modification. If the sources are pristine,
+# then the timestamp will match that of the SCM's most recent modification
+# date.
+timestamp() {
+	local t
 
-if [ -d ".git" ]; then
-  IS_GIT=1
-elif [ -d ".svn" ]; then
-  IS_SVN=1
-fi
+	# date syntaxes are manifold:
+	# gnu		date [-d input]... [+FORMAT]
+	# netbsd	date [-ajnu] [-d date] [-r seconds] [+format] [[[[[[CC]yy]mm]dd]HH]MM[.SS]]
+	# freebsd	date [-jnu]  [-d dst] [-r seconds] [-f fmt date | [[[[[cc]yy]mm]dd]HH]MM[.ss]] [+format] [...]
+	# dragonflybsd	date [-jnu]  [-d dst] [-r seconds] [-f fmt date | [[[[[cc]yy]mm]dd]HH]MM[.ss]] [+format] [...]
+	# openbsd	date [-aju]  [-d dst] [-r seconds] [+format] [[[[[[cc]yy]mm]dd]HH]MM[.SS]] [...]
+	if git_is_file_tracked "$2" ; then
+		# are there local changes?
+		if git_has_local_changes "$2" ; then
+			t=$(date -u "${1}")
+		else
+			# No local changes, get date of the last commit
+			case $(uname) in
+			# Most BSD dates do not support parsing date values from user input with -d but all of
+			# them support parsing epoch seconds with -r. Thanks to git we can easily use that:
+			NetBSD|OpenBSD|DragonFly|FreeBSD)
+				t=$(date -u -r "$(git log --pretty=format:%ct -1 -- $2)"  "$1" 2>/dev/null);;
+			*)
+				t=$(date -d "$(git log --pretty=format:%cD -1 -- $2)" -u "$1" 2>/dev/null);;
+			esac
+		fi
+	else
+		t=$(date -u "$1")
+	fi
 
-get_revision() {
-  if [ $IS_GIT -eq 1 ]; then
-    echo "$(git_revision)"
-  elif [ $IS_SVN -eq 1 ]; then
-    echo "$(svn_revision)"
-  fi
+	if [ -z "$t" ]; then
+		echo "Warning: Could not determine timestamp." 2>/dev/null
+	fi
+	echo "${t}"
 }
 
-get_server() {
-  if [ $IS_GIT -eq 1 ]; then
-    echo "$(git_server)"
-  elif [ $IS_SVN -eq 1 ]; then
-    echo "$(svn_server)"
-  fi
+revision() {
+	local r
+	if git_is_file_tracked "$1" ; then
+		r=$(git describe $(git_last_commit "$1"))
+		if git_has_local_changes "$1" ; then
+			r="$r-dirty"
+		fi
+	else
+		r="unknown"
+	fi
+
+	echo "${r}"
 }
 
-get_timestamp() {
-  if [ $IS_GIT -eq 1 ]; then
-    echo "$(git_timestamp)"
-  elif [ $IS_SVN -eq 1 ]; then
-    echo "$(svn_timestamp)"
-  fi
+is_tracked() {
+	is_file_tracked "$1"
 }
 
 show_help() {
-  echo "Usage: getrevision.sh <options>"
-  echo ""
-  echo "Options:"
-  echo -e "\t-r | --revision     Revision (SVN), commit hash (Git)"
-  echo -e "\t-s | --server       Upstream server"
-  echo -e "\t-t | --timestamp    UTC timestamp of most recent modification"
+	echo "Usage:
+	${0} <command> [path]
+
+Commands
+    -h or --help
+        this message
+    -c or --check
+        test if path is under version control at all
+    -r or --revision
+        return unique revision information including an indicator for
+        uncommitted changes
+    -U or --url
+        URL associated with the latest commit
+    -d or --date
+        date of most recent modification
+    -t or --timestamp
+        timestamp of most recent modification
+"
+	return
 }
 
-if [ -z "$*" ]; then
-  # default action: print "<revision> : <server> : <timestamp>"
-  echo "${0}: Using default parameters" >&2
-  echo "$(get_server) : $(get_revision) : $(get_timestamp)"
-else
-  for ARG in $@; do
-    case ${ARG} in
-      -h|--help)
-        show_help;
-        shift;;
-      -r|--revision)
-        echo "$(get_revision)"
-        shift;;
-      -s|--server)
-        echo "$(get_server)"
-        shift;;
-      -t|--timestamp)
-        echo "$(get_timestamp)"
-        shift;;
-    esac;
-  done
-fi
+check_action() {
+	if [ -n "$action" ]; then
+		echo "Error: Multiple actions given.">&2
+		exit ${EXIT_FAILURE}
+	fi
+}
+
+main() {
+	local query_path=
+	local action=
+
+	# Argument parser loop
+	while [ $# -gt 0 ];
+	do
+		case ${1} in
+		-h|--help)
+			action=show_help;
+			shift;;
+		-U|--url)
+			check_action $1
+			action=scm_url
+			shift;;
+		-d|--date)
+			check_action $1
+			action="timestamp +%Y-%m-%d" # refrain from suffixing 'Z' to indicate it's UTC
+			shift;;
+		-r|--revision)
+			check_action $1
+			action=revision
+			shift;;
+		-t|--timestamp)
+			check_action $1
+			action="timestamp +%Y-%m-%dT%H:%M:%SZ" # There is only one valid time format! ISO 8601
+			shift;;
+		-c|--check)
+			check_action $1
+			action=is_tracked
+			shift;;
+		-*)
+			show_help;
+			echo "Error: Invalid option: ${1}"
+			exit ${EXIT_FAILURE};;
+		*)
+			if [ -z "$query_path" ] ; then
+				if [ ! -e "$1" ] ; then
+					echo "Error: Path \"${1}\" does not exist.">&2
+					exit ${EXIT_FAILURE}
+				fi
+				query_path=$1
+			else
+				echo "Warning: Ignoring overabundant parameter: \"${1}\"">&2
+			fi
+			shift;;
+		esac;
+	done
+
+	# default to current directory (usually equals the whole repository)
+	if [ -z "$query_path" ] ; then
+		query_path=.
+	fi
+	if [ -z "$action" ] ; then
+		show_help
+		echo "Error: No actions specified"
+		exit ${EXIT_FAILURE}
+	fi
+
+	$action "$query_path"
+}
+
+main $@
