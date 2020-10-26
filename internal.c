4804771f84f92111e6e6fb5cb3865306e7042cfc
Edward O'Callaghan <quasisec@google.com>
2020-10-26 19:07:25 +1100

    BACKPORT: internal.c: Co-locate global variables to top of file
    This just makes internal.c at bit easier to parse and helps
    the read get a view of all the singleton state in one go.
    
    BUG=none
    BRANCH=none
    TEST=builds
    
    Original-Change-Id: Id4109dfb17f63d80fb3fb3f2a1d0ab54d9eddc6e
    Original-Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Original-Reviewed-on: https://review.coreboot.org/c/flashrom/+/46814
    Original-Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Original-Reviewed-by: Angel Pons <th3fanbus@gmail.com>
    (cherry picked from commit c15de25a7ca6504a4191f8da44448a0723923e58)
    
    Change-Id: I0f6757605774b6b7da6a6d28da04ea373ced5018
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2512925
    Reviewed-by: Alan Green <avg@chromium.org>
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Commit-Queue: Alan Green <avg@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>

--- internal.c
+++ internal.c
@@ -23,6 +23,14 @@
 #include "programmer.h"
 #include "hwaccess.h"
 
+int is_laptop = 0;
+int laptop_ok = 1;
+
+int force_boardenable = 0;
+int force_boardmismatch = 0;
+
+enum chipbustype internal_buses_supported = BUS_NONE;
+
 struct pci_dev *pci_dev_find_vendorclass(uint16_t vendor, uint16_t devclass)
 {
 	struct pci_dev *temp;
@@ -81,9 +89,6 @@ struct pci_dev *pci_card_find(uint16_t vendor, uint16_t device,
 	return NULL;
 }
 
-int force_boardenable = 0;
-int force_boardmismatch = 0;
-
 #if IS_X86
 void probe_superio(void)
 {
@@ -111,9 +116,6 @@ int register_superio(struct superio s)
 }
 
 #endif // if IS_X86
-
-int is_laptop = 0;
-int laptop_ok = 1;	/* FIXME: proper whitelisting hasn't been added yet */
 
 static void internal_chip_writeb(const struct flashctx *flash, uint8_t val,
 				 chipaddr addr)
@@ -168,8 +170,6 @@ static const struct par_master par_master_internal = {
 		.chip_writel		= internal_chip_writel,
 		.chip_writen		= fallback_chip_writen,
 };
-
-enum chipbustype internal_buses_supported = BUS_NONE;
 
 enum chipbustype target_bus;
 
