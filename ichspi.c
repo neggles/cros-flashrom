neggles/cros-flashrom

commit 69c3a0df8cc39c876dddfd26a490d7da54533a43

ichspi.c: Deduplicate check_fd_permissions_hwseq() logic
check_fd_permissions_hwseq() is almost exactly the same as
check_fd_permissions() so deduplicate the code.

BUG=b:156870835,b:155142726,b:139255749,b:139258259
BRANCH=none
TEST=builds

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: Iaf0fb8828f1994ed3f253e3611982f2bba67191f
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2315557
Tested-by: Edward O'Callaghan <quasisec@chromium.org>
Reviewed-by: Sam McNally <sammc@chromium.org>
Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

ichspi.c
@@ -1211,58 +1211,12 @@ struct fd_region {
 	{ .name = "Embedded Controller" },
 };
 
-static int check_fd_permissions_hwseq(int op_type, uint32_t addr, int count)
+static int check_fd_permissions(OPCODE *opcode, int type, uint32_t addr, int count)
 {
 	int i;
-	int ret = 0;
-
-	/* check flash descriptor permissions (if present) */
-	for (i = 0; i < num_fd_regions; i++) {
-		const char *name = fd_regions[i].name;
-		enum fd_access_level level;
-
-		if ((addr + count - 1 < fd_regions[i].base) ||
-		    (addr > fd_regions[i].limit))
-			continue;
-
-		if (!fd_regions[i].permission) {
-			msg_perr("No permissions set for flash region %s\n",
-				  fd_regions[i].name);
-			break;
-		}
-		level = fd_regions[i].permission->level;
-
-		if (op_type == HWSEQ_READ) {
-			if (level != FD_REGION_READ_ONLY &&
-			    level != FD_REGION_READ_WRITE) {
-				msg_pspew("%s: Cannot read address 0x%08x in "
-					"region %s\n", __func__, addr, name);
-				ret = SPI_ACCESS_DENIED;
-			}
-		} else if (op_type == HWSEQ_WRITE) {
-			if (level != FD_REGION_WRITE_ONLY &&
-			    level != FD_REGION_READ_WRITE) {
-				msg_pspew("%s: Cannot write to address 0x%08x "
-					"in region %s\n", __func__, addr, name);
-				ret = SPI_ACCESS_DENIED;
-			}
-		}
-		break;
-	}
-
-	if (i == num_fd_regions) {
-		msg_pspew("%s: Address not covered by any descriptor 0x%06x\n",
-			  __func__, addr);
-		ret = SPI_ACCESS_DENIED;
-	}
-
-	return ret;
-}
-
-static int check_fd_permissions(OPCODE *opcode, uint32_t addr, int count)
-{
-	int i;
-	uint8_t type = opcode->spi_type;
+	uint8_t op_type = opcode ? opcode->spi_type : type;
+	int op_type_r = opcode ? SPI_OPCODE_TYPE_READ_WITH_ADDRESS : HWSEQ_READ;
+	int op_type_w = opcode ? SPI_OPCODE_TYPE_WRITE_WITH_ADDRESS : HWSEQ_WRITE;
 	int ret = 0;
 
 	/* check flash descriptor permissions (if present) */
@@ -1282,14 +1236,14 @@ static int check_fd_permissions(OPCODE *opcode, uint32_t addr, int count)
 
 		level = fd_regions[i].permission->level;
 
-		if (type == SPI_OPCODE_TYPE_READ_WITH_ADDRESS) {
+		if (op_type == op_type_r) {
 			if (level != FD_REGION_READ_ONLY &&
 			    level != FD_REGION_READ_WRITE) {
 				msg_pspew("%s: Cannot read address 0x%08x in "
 				          "region %s\n", __func__,addr,name);
 				ret = SPI_ACCESS_DENIED;
 			}
-		} else if (type == SPI_OPCODE_TYPE_WRITE_WITH_ADDRESS) {
+		} else if (op_type == op_type_w) {
 			if (level != FD_REGION_WRITE_ONLY &&
 			    level != FD_REGION_READ_WRITE) {
 				msg_pspew("%s: Cannot write to address 0x%08x in"
@@ -1300,7 +1254,18 @@ static int check_fd_permissions(OPCODE *opcode, uint32_t addr, int count)
 		break;
 	}
 
+	if (i == num_fd_regions) {
+		msg_pspew("%s: Address not covered by any descriptor 0x%06x\n",
+			  __func__, addr);
+		ret = SPI_ACCESS_DENIED;
+	}
+
 	return ret;
+}
+
+static int check_fd_permissions_hwseq(int op_type, uint32_t addr, int count)
+{
+	return check_fd_permissions(NULL, op_type, addr, count);
 }
 
 static int ich_spi_send_command(const struct flashctx *flash, unsigned int writecnt,
@@ -1415,7 +1380,7 @@ static int ich_spi_send_command(const struct flashctx *flash, unsigned int write
 		addr += addr_offset;
 
 		if (num_fd_regions > 0) {
-			result = check_fd_permissions(opcode, addr, count);
+			result = check_fd_permissions(opcode, 0, addr, count);
 			if (result)
 				return result;
 		}

commit 93efced602f2e542ec39f2389935b4306c79e9ce

ichspi.c: Restore previous behaviour of check_fd_permissions_hwseq()
Workaround a regression from the commit
69c3a0df8cc39c876dddfd26a490d7da54533a43.

check_fd_permissions_hwseq() && check_fd_permissions() were meant
to be dedup however there is a slight difference in logic which causes
issue when the descriptors are not correct for the chipset in
chipset_enable.c. Since our chipset_enable.c is slightly different
from upstream and we have abused some chip wrapper callbacks as other
families this causes issues with the descriptors. This winds up breaking
things when the stars align between this commit and the nature of our
chipset_enable.c

This only turns up in very specific cases when the firmware updater is
writing in a very specific pattern to SPI flash. The issue cannot
necessarily be repro'ed simply doing a full erase then write cycle,
atleast not reliably.

BUG=b:171892105,b:156870835,b:155142726,b:139255749,b:139258259
BRANCH=none
TEST=repro on Edgar and resolve's issue.

Signed-off-by: Edward O'Callaghan <quasisec@google.com>
Change-Id: I48617c9ebf28cc6cc44d98e52688d7611a453622
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2517210
Tested-by: Edward O'Callaghan <quasisec@chromium.org>
Reviewed-by: Sam McNally <sammc@chromium.org>
Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

ichspi.c
@@ -1222,7 +1222,7 @@ static int check_fd_permissions(OPCODE *opcode, int type, uint32_t addr, int cou
 		break;
 	}
 
-	if (i == num_fd_regions) {
+	if ((i == num_fd_regions) && !opcode) { // FIXME(b/171892105).
 		msg_pspew("%s: Address not covered by any descriptor 0x%06x\n",
 			  __func__, addr);
 		ret = SPI_ACCESS_DENIED;