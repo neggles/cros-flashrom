16b62b87df41ea03538fff2054a629998182b780
Edward O'Callaghan <quasisec@google.com>
2020-11-16 11:10:48 +1100

    chipset_enable.c: Add cases for Lewisburg and Cannon Point
    Modelled off upstream code, this also makes follow up diffs easier to
    understand.
    
    BUG=b:173164205
    BRANCH=none
    TEST=Spot checked across a number of Intel DUT's with full R->E->W
    cycles.
    
    Signed-off-by: Edward O'Callaghan <quasisec@google.com>
    Change-Id: I8a563ac2852f4a71d9f9c004c147fd64457e5d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2536521
    Tested-by: Edward O'Callaghan <quasisec@chromium.org>
    Auto-Submit: Edward O'Callaghan <quasisec@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Edward O'Callaghan <quasisec@chromium.org>

--- chipset_enable.c
+++ chipset_enable.c
@@ -763,6 +763,8 @@ static int enable_flash_ich_spi(struct pci_dev *dev, enum ich_chipset ich_genera
 		top_swap = (gcs & 2) >> 1;
 		break;
 	case CHIPSET_100_SERIES_SUNRISE_POINT:
+	case CHIPSET_C620_SERIES_LEWISBURG:
+	case CHIPSET_300_SERIES_CANNON_POINT:
 	case CHIPSET_APOLLO_LAKE:
 		reg_name = "BIOS_SPI_BC";
 		bild = (gcs >> 7) & 1;
