neggles/cros-flashrom

commit 25a4ca21467783198c48474796d6c21a8fd4e08b

Handle read-only SPI regions
If a region of the SPI flash cannot be written, then it should not be
checked against the original when it is time to verify.

The erase/write commands already handle "access denied" errors without
failing so this just extends the same support to verify.

In order to plumb this through I added a new "check_access()" function
that can be set for the flash chip.  It is only implemented for the
opaque hwseq programmer.

BUG=b:130706387
BRANCH=noe
TEST=ensure flashrom does not fail when EC region is read only

Change-Id: I4524c75105f1e2d3ee0f1bdd882c2ee1d415c0d9
Signed-off-by: Duncan Laurie <dlaurie@google.com>
Reviewed-on: https://chromium-review.googlesource.com/1584648
Commit-Ready: Stefan Reinauer <reinauer@google.com>
Reviewed-by: Furquan Shaikh <furquan@chromium.org>

opaque.c
@@ -36,6 +36,7 @@ struct opaque_programmer opaque_programmer_none = {
 	.read_status = NULL,
 	.write_status = NULL,
 	.erase = NULL,
+	.check_access = NULL,
 };
 
 struct opaque_programmer *opaque_programmer = &opaque_programmer_none;
@@ -105,6 +106,13 @@ int write_status_opaque(const struct flashctx *flash, int status)
 		return 1;
 	}
 	return opaque_programmer->write_status(flash, status);
+}
+
+int check_access_opaque(const struct flashctx *flash, unsigned int start, unsigned int len, int rw)
+{
+	if (opaque_programmer->check_access)
+		return opaque_programmer->check_access(flash, start, len, rw);
+	return 1;
 }
 
 void register_opaque_programmer(struct opaque_programmer *pgm)

chipdrivers.h
@@ -107,6 +107,7 @@ int write_opaque(struct flashctx *flash, const uint8_t *buf, unsigned int start,
 int erase_opaque(struct flashctx *flash, unsigned int blockaddr, unsigned int blocklen);
 uint8_t read_status_opaque(const struct flashctx *flash);
 int write_status_opaque(const struct flashctx *flash, int status);
+int check_access_opaque(const struct flashctx *flash, unsigned int start, unsigned int len, int read);
 
 /* 82802ab.c */
 uint8_t wait_82802ab(struct flashctx *flash);

flash.h
@@ -210,6 +210,7 @@ struct flashchip {
 	int (*read) (struct flashctx *flash, uint8_t *buf, unsigned int start, unsigned int len);
 	uint8_t (*read_status) (const struct flashctx *flash);
 	int (*write_status) (const struct flashctx *flash, int status);
+	int (*check_access) (const struct flashctx *flash, unsigned int start, unsigned int len, int read);
 	struct voltage_range voltage;
 	struct wp *wp;
 };

flashchips.c
@@ -11112,6 +11112,7 @@ const struct flashchip flashchips_hwseq[] = {
 		.read		= read_opaque,
 		.read_status	= read_status_opaque,
 		.write_status	= write_status_opaque,
+		.check_access	= check_access_opaque,
 		.wp		= &wp_w25,
 		.unlock	= &spi_disable_blockprotect,
 	},

flashrom.c
@@ -38,6 +38,7 @@
 #include "flashchips.h"
 #include "layout.h"
 #include "programmer.h"
+#include "spi.h"
 
 const char flashrom_version[] = FLASHROM_VERSION;
 char *chip_to_probe = NULL;
@@ -663,6 +664,16 @@ int verify_range(struct flashctx *flash, uint8_t *cmpbuf, unsigned int start, un
 					continue;
 				else
 					goto out_free;
+			}
+
+			/*
+			 * Check write access permission and do not compare chunks
+			 * where flashrom does not have write access to the region.
+			 */
+			if (flash->chip->check_access) {
+				tmp = flash->chip->check_access(flash, start + i, chunksize, 0);
+				if (tmp && ignore_error(tmp))
+					continue;
 			}
 
 			failcount = compare_chunk(readbuf + i, cmpbuf + i, start + i,

ichspi.c
@@ -1782,6 +1782,12 @@ int pch100_hwseq_block_erase(struct flashctx *flash,
 	return 0;
 }
 
+int pch100_hwseq_check_access(const struct flashctx *flash, unsigned int start,
+			      unsigned int len, int read)
+{
+	return check_fd_permissions_hwseq(read ? HWSEQ_READ : HWSEQ_WRITE, start, len);
+}
+
 int pch100_hwseq_read(struct flashctx *flash, uint8_t *buf, unsigned int addr,
 		   unsigned int len)
 {
@@ -2136,6 +2142,7 @@ static struct opaque_programmer opaque_programmer_pch100_hwseq = {
 	.read_status = pch100_hwseq_read_status,
 	.write_status = pch100_hwseq_write_status,
 	.erase = pch100_hwseq_block_erase,
+	.check_access = pch100_hwseq_check_access,
 };
 
 static struct opaque_programmer opaque_programmer_ich_hwseq = {

programmer.h
@@ -669,6 +669,7 @@ struct opaque_programmer {
 	int (*erase) (struct flashctx *flash, unsigned int blockaddr, unsigned int blocklen);
 	uint8_t (*read_status) (const struct flashctx *flash);
 	int (*write_status) (const struct flashctx *flash, int status);
+	int (*check_access) (const struct flashctx *flash, unsigned int start, unsigned int len, int read);
 	const void *data;
 };
 extern struct opaque_programmer *opaque_programmer;

commit 11acc1fe0a4a76e8e272dc7443c1557090a18413

CHROMIUM: opaque.c: Don't default to "no access" if check_access not implemented
The check_access() function's return value is a bit ambiguous.  Does
it return "0" for no access and "1" for access OK or does it return
something else?  By majority rules, it appears that the answer is that
it return "0" for "no access errors" or some type of error code
indicating what type of access problem there was.  Tally of the votes:
* check_access_opaque() returned "1" if check_access() wasn't
  implemented. The only sane thing to return if check_access() is not
  implemented is "access OK", so I take this as a vote for "1" being
  access OK and "0" being no access.
* The verify_range() function, a client of check_access(), made the
  assumption that "0" meant access OK and "non-zero" meant an access
  problem.  It turned out not to matter _too_ much (no errors were
  reported) since the the ignore_error() function would see "1" as an
  unrecognized error code and decide it wasn't an error it cared
  about.
* The ich_hwseq_check_access() function, an implementor of
  check_access(), was clearly returning an error code or "0" if no
  errors.

Let's fix the odd one out.  check_access_opaque() will now return "0"
(access OK) if check_access() isn't implemented.

NOTE: the code being touched here appears downstream only, hence
posting my fix as CHROMIUM.  See CL:1584648

BRANCH=trogdor
BUG=b:178822068
TEST=Speed test flashrom write

Fixes: 25a4ca214677 ("Handle read-only SPI regions")
Signed-off-by: Douglas Anderson <dianders@chromium.org>
Change-Id: Ic54ff561eb956d519c63b7c0c4c34355db978807
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2666368
Reviewed-by: Furquan Shaikh <furquan@chromium.org>
Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

opaque.c
@@ -64,7 +64,7 @@ int check_access_opaque(const struct flashctx *flash, unsigned int start, unsign
 {
 	if (flash->mst->opaque.check_access)
 		return flash->mst->opaque.check_access(flash, start, len, rw);
-	return 1;
+	return 0;
 }
 
 int register_opaque_master(const struct opaque_master *mst)