8e0145b12c1b96eb22945f6d3ccf5f2fdd08c464
Nikolai Artemiev <nartemiev@google.com>
2021-03-01 17:11:27 +1100

    flashchips.c: delete duplicate MT25QL256 chip
    A newer version of this chip was imported from upstream
    in `commit 09a5eac88`, making it a duplicate.
    
    BUG=b:166294558
    BRANCH=none
    TEST=builds
    
    Signed-off-by: Nikolai Artemiev <nartemiev@google.com>
    Change-Id: Ibd1e3a671ca5a07d903d0e5665b74cf99955a432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2725870
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -10879,39 +10879,6 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Micron/Numonyx/ST",
-		.name		= "MT25QL256", /* ..3E = 3V, uniform 64KB/4KB blocks/sectors */
-		.bustype	= BUS_SPI,
-		.manufacture_id	= ST_ID,
-		.model_id	= ST_N25Q256__3E,
-		.total_size	= 32768,
-		.page_size	= 256,
-		/* supports SFDP */
-		/* OTP: 64B total; read 0x4B, write 0x42 */
-		.feature_bits	= FEATURE_WRSR_WREN | FEATURE_OTP | FEATURE_4BA_WREN,
-		.tested		= TEST_OK_PREW,
-		.probe		= probe_spi_rdid,
-		.probe_timing	= TIMING_ZERO,
-		.block_erasers	=
-		{
-			{
-				.eraseblocks = { {4 * 1024, 8192} },
-				.block_erase = spi_block_erase_21,
-			}, {
-				.eraseblocks = { {64 * 1024, 512} },
-				.block_erase = spi_block_erase_dc,
-			}, {
-				.eraseblocks = { {32768 * 1024, 1} },
-				.block_erase = spi_block_erase_c7,
-			}
-		},
-		.unlock		= spi_disable_blockprotect, /* TODO: per 64kB sector lock registers */
-		.write		= spi_chip_write_256, /* Multi I/O supported */
-		.read		= spi_chip_read, /* Fast read (0x0B) and multi I/O supported */
-		.voltage	= {2700, 3600},
-	},
-
-	{
-		.vendor		= "Micron/Numonyx/ST",
 		.name		= "MT25QL512", /* ..3E = 3V, uniform 64KB/4KB blocks/sectors */
 		.bustype	= BUS_SPI,
 		.manufacture_id	= ST_ID,
