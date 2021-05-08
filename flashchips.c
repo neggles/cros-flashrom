4b0e134055808048f418018ddbb0bc8009aed320
Nikolai Artemiev <nartemiev@google.com>
2021-05-08 17:15:02 +1000

    flashchips.c: delete duplicate chip entry
    Delete duplicate chip that is not present in upstream.
    
    BUG=b:166294558
    BRANCH=none
    TEST=builds
    
    Change-Id: I31daa70d79807a16c031faeee815390be875db18
    Signed-off-by: Nikolai Artemiev <nartemiev@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/flashrom/+/2879777
    Reviewed-by: Edward O'Callaghan <quasisec@chromium.org>

--- flashchips.c
+++ flashchips.c
@@ -9194,44 +9194,6 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Macronix",
-		.name		= "MX25L8005/MX25L8006E/MX25L8008E/MX25V8005",
-		.bustype	= BUS_SPI,
-		.manufacture_id	= MACRONIX_ID,
-		.model_id	= MACRONIX_MX25L8005,
-		.total_size	= 1024,
-		.page_size	= 256,
-		.feature_bits	= FEATURE_WRSR_WREN,
-		.tested		= TEST_OK_PREW,
-		.probe		= probe_spi_rdid,
-		.probe_timing	= TIMING_ZERO,
-		.block_erasers	=
-		{
-			{
-				.eraseblocks = { {4 * 1024, 256} },
-				.block_erase = spi_block_erase_20,
-			}, {
-				.eraseblocks = { {64 * 1024, 16} },
-				.block_erase = spi_block_erase_52,
-			}, {
-				.eraseblocks = { {64 * 1024, 16} },
-				.block_erase = spi_block_erase_d8,
-			}, {
-				.eraseblocks = { {1024 * 1024, 1} },
-				.block_erase = spi_block_erase_60,
-			}, {
-				.eraseblocks = { {1024 * 1024, 1} },
-				.block_erase = spi_block_erase_c7,
-			},
-		},
-		.printlock	= spi_prettyprint_status_register_bp2_srwd,
-		.unlock		= spi_disable_blockprotect,
-		.write		= spi_chip_write_256,
-		.read		= spi_chip_read,
-		.voltage	= {2700, 3600},
-	},
-
-	{
-		.vendor		= "Macronix",
 		.name		= "MX25R6435F",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= MACRONIX_ID,
